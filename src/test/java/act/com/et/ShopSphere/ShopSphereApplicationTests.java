5729f14932c58539c444e705022e25823a52c3eb
getuk <getuk@act.com.et>
2024-11-12 12:26:53 +0300

    initial commit

---
+++ src/test/java/act/com/et/ShopSphere/ShopSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package act.com.et.ShopSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
